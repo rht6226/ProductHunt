a36c598d4dc9d15efab790bb8bf600de4ee5708f
Rohit Raj Anand <rohitraazanand567@gmail.com>
2018-08-24 20:22:23 +0530

    LogIn/Logout Working, Dynamic Navbar

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
-def login(request):
-    return render(request, 'login.html')
-
-def logout(request):
-    return render(request, 'logout.html')
 
 def signup(request):
     if request.method == 'POST':
@@ -26,3 +21,19 @@ def signup(request):
     else:
         #other stuff
         return render(request, 'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username = request.POST['username'], password= request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('main')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid Credentials! Please enter correct username and password.'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('main')
