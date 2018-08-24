029f9a11ebbf703b58f27135a9048d3155c84fbc
Rohit Raj Anand <rohitraazanand567@gmail.com>
2018-08-24 17:41:36 +0530

    signup working'

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    return render(request, 'logout.html')
+
+def signup(request):
+    if request.method == 'POST':
+        #USER wants to sign up
+        if request.POST['password1'] == request.POST['password2']:
+            #Entered passwords are identical
+            try:
+                #check if user already exists in database if yes raise error
+                user = User.objects.get(username = request.POST['username'])
+                return render(request, 'signup.html', {'error': 'Username is already taken!'})
+            except User.DoesNotExist:
+                #username is available
+                user = User.objects.create_user(username =request.POST['username'], password= request.POST['password1'])
+                auth.login(request, user)
+                return redirect('main')
+
+    else:
+        #other stuff
+        return render(request, 'signup.html')
--- producthunt/urls.py
+++ producthunt/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 import products.views
 from django.conf.urls.static import static
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.main, name='main'),
+    path('accounts/', include('accounts.urls')),
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
